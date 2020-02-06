197985df4d5bcf4730ef3094fed2e97ca99f0916
Vasiliy <vbbatmaev@gmail.com>
2020-02-06 11:55:07 +0300

    mvp

--- transform.js
+++ transform.js
@@ -17,6 +17,7 @@ keyObj.addEventListener("keypress", _onKeyPress.bind(this))
 
 function _onKeyPress(event) {
     if(event.keyCode === 13){
+        event.preventDefault()
         form.dispatchEvent(new Event("submit"))
     }
 }
@@ -39,5 +40,48 @@ function _onSubmit(event){
 }
 
 function trans(str){
-    return str
+    let array = str.split("")
+    //alert(array)
+    // array.map(item => item + item)
+    // return array.join(" ")
+    array = array.map(item => item.charCodeAt(0))
+    //alert(array)
+    array = array.map(item => unicodeToMyCode(item))
+    //alert(array)
+    array = array.map(item => item ^ (rand() % 128))
+    //alert(array)
+    array = array.map(item => myCodeToUnicode(item))
+    //alert(array)
+    array = array.map(item => String.fromCharCode(item))
+    //alert(array)
+    return array.join("")
+}
+
+function unicodeToMyCode(n){
+    if(n < 31){
+        alert(`Похоже, вы ввели недопустимый символ с кодом ${n}. Автор сайта предполагал, что это невозможно.`)
+        return false
+    } else if(n < 128){
+        return n - 32
+    } else if(n >= 1040 && n <= 1103){
+        return n - 944
+    } else {
+        alert(`Жаль, но символ ${String.fromCharCode(n)} не поддерживается`)
+        return false
+    }
+}
+
+function myCodeToUnicode(n){
+    if(n < 0){
+        return false
+    }
+    else if(n <= 95){
+        return n + 32
+    }
+    else if(n <= 159){
+        return n + 944
+    }
+    else{
+        return false
+    }
 }
