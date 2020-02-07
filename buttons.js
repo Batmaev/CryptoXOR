4e8f6f4d45875472a6db90faa681e5bc839239a4
Vasiliy <vbbatmaev@gmail.com>
2020-02-07 14:19:16 +0300

    случайный ключ

--- buttons.js
+++ buttons.js
@@ -4,3 +4,9 @@ function copy(elem){
     document.execCommand("copy")
     window.getSelection().removeAllRanges()
 }
+
+function randomKey(){
+    let a = new Uint32Array(1)
+    window.crypto.getRandomValues(a)
+    keyObj.value = a[0]
+}
