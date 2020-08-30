295ce84ecaab7e542f589f2275c232777ff79204
Василий <vbbatmaev@gmail.com>
2020-08-30 23:43:05 +0300

    вроде переписанный css работает

--- align.js
+++ align.js
@@ -1,17 +1,3 @@
-const before = document.getElementById("before")
-const after = document.getElementById("after")
-
-const beforePosition = before.getBoundingClientRect()
-const afterPosition = after.getBoundingClientRect()
-
-if(beforePosition.top < afterPosition.top){
-    if(beforePosition.width == 400){ //магическая константа = width + padding + padding
-        const container = document.getElementById("before-and-after")
-        container.style.flexDirection = "column"
-    }
-    before.style.maxHeight = "35vh"
-    after.style.maxHeight = "35vh"
-
-    const keyetc = document.getElementById("key-etc")
-    keyetc.style.maxWidth = "400px"
+if(after.getBoundingClientRect().top - before.getBoundingClientRect().top > 10){
+    before.style.maxHeight = after.style.maxHeight = "40vh"
 }
